677e182de13060b0c2c21a4ab8d8d6f4406694f1
Yusei36 <15942327+yusei36@users.noreply.github.com>
2023-08-13 17:25:18 +0200

    use SimpleInjector

--- KeePassEntrySearcherWpf/App.xaml.cs
+++ KeePassEntrySearcherWpf/App.xaml.cs
@@ -1,10 +1,12 @@
-﻿using KeePassEntrySearcherWpf.Views;
-using KeePassEntrySearcherWpf.ViewModels;
-using NHotkey.Wpf;
+﻿using System.Windows;
 using System.Windows.Input;
 using NHotkey;
-using System.Windows;
+using NHotkey.Wpf;
+using SimpleInjector;
+using KeePassEntrySearcherWpf.Views;
+using KeePassEntrySearcherWpf.ViewModels;
 using KeePassEntrySearcherContracts;
+using KeePassEntrySearcherContracts.Services;
 using KeePassEntrySearcherWpf.Services;
 
 namespace KeePassEntrySearcherWpf
@@ -14,16 +16,39 @@ namespace KeePassEntrySearcherWpf
     /// </summary>
     public partial class App
     {
-        private SearchWindow searchWindow;
+        private SearchWindow? searchWindow;
+        private Container? container;
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+
+            container = new Container();
+            container.Register<SearchWindow, SearchWindow>();
+            container.Register<SearchWindowViewModel, SearchWindowViewModel>();
+
+            container.Register<IEntryActionService, EntryActionService>();
+            container.Register<IEntrySearchService, EntrySearchService>();
         }
 
         public void Init(IKeePassDataProvider dataProvider, IKeePassInteractionManager interactionManager)
         {
-            var entryActionSerivce = new EntryActionService(interactionManager, dataProvider);
-            searchWindow = new SearchWindow(new SearchWindowViewModel(dataProvider, interactionManager, new EntrySearchService(), entryActionSerivce));
+            if (dataProvider == null)
+            {
+                throw new ArgumentNullException(nameof(dataProvider));
+            }
+            if (interactionManager == null)
+            {
+                throw new ArgumentNullException(nameof(interactionManager));
+            }
+            if (container == null)
+            {
+                throw new NullReferenceException("Container shouldn't be null while initializing");
+            }
+
+            container.RegisterInstance(dataProvider);
+            container.RegisterInstance(interactionManager);
+
+            searchWindow = container.GetInstance<SearchWindow>();
             MainWindow = searchWindow;
 
             HotkeyManager.Current.AddOrReplace(nameof(ShowSearchWindow), Key.F, ModifierKeys.Control | ModifierKeys.Alt, ShowSearchWindow);
