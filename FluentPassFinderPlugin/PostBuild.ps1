ce4aefc43bf6918bb138839d145ae0b386f57618
Yusei36 <15942327+yusei36@users.noreply.github.com>
2023-08-25 17:25:38 +0200

    moved PostBuild.ps1 into Plugin project

--- FluentPassFinderPlugin/PostBuild.ps1
+++ FluentPassFinderPlugin/PostBuild.ps1
@@ -11,6 +11,7 @@ New-Item -ItemType directory -Name bin -Path $pluginOutputPath
 
 Copy-Item $OutputSrcDir/*.dll -Exclude KeePass* -Destination $pluginOutputPath
 Copy-Item $OutputSrcDir/bin/*.dll -Exclude KeePass* -Destination $pluginOutputPath/bin/
+
 if ($Configuration -eq "Debug") {
     Copy-Item $OutputSrcDir/*.pdb -Destination $pluginOutputPath
     Copy-Item $OutputSrcDir/bin/FluentPassFinder* -Destination $pluginOutputPath/bin/
