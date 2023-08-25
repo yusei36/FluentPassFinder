db1bc67d2fa4e6854b7ca3f94b864953ed206d76
Yusei36 <15942327+yusei36@users.noreply.github.com>
2023-08-25 17:10:17 +0200

    updated build process

--- Build/PostBuild.ps1
+++ Build/PostBuild.ps1
@@ -1,6 +1,6 @@
-param([string] $OutputSrcDir, [string] $SolutionDir)
+param([string] $OutputSrcDir, [string] $SolutionDir, [string] $Configuration)
 
-$buildPath = "$SolutionDir/Build"
+$buildPath = "$SolutionDir/Build/KeePass/Plugins"
 $pluginOutputPath = "$buildPath/FluentPassFinder"
 if ((Test-Path -Path $pluginOutputPath )) {
     Remove-Item -Recurse $pluginOutputPath
@@ -11,4 +11,10 @@ New-Item -ItemType directory -Name bin -Path $pluginOutputPath
 
 Copy-Item $OutputSrcDir/*.dll -Exclude KeePass* -Destination $pluginOutputPath
 Copy-Item $OutputSrcDir/bin/*.dll -Exclude KeePass* -Destination $pluginOutputPath/bin/
-Copy-Item $OutputSrcDir/bin/FluentPassFinder* -Exclude *.pdb -Destination $pluginOutputPath/bin/
+if ($Configuration -eq "Debug") {
+    Copy-Item $OutputSrcDir/*.pdb -Destination $pluginOutputPath
+    Copy-Item $OutputSrcDir/bin/FluentPassFinder* -Destination $pluginOutputPath/bin/
+}
+else {
+    Copy-Item $OutputSrcDir/bin/FluentPassFinder* -Exclude *.pdb -Destination $pluginOutputPath/bin/
+}
