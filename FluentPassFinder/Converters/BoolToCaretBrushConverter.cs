364962d4f7f224cdbf717bafe973953126d0451e
Yusei36 <15942327+yusei36@users.noreply.github.com>
2026-04-05 02:19:46 +0200

    refine ui

---
+++ FluentPassFinder/Converters/BoolToCaretBrushConverter.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using System.Globalization;
+
+namespace FluentPassFinder.Converters
+{
+    public class BoolToCaretBrushConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is true)
+                return Brushes.Transparent;
+
+            return AvaloniaProperty.UnsetValue; // fallback to default theme brush
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
