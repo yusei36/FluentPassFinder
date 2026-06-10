c90b6b5045f2b5e1ccc44a92b9931b2098029b54
Yusei36 <15942327+yusei36@users.noreply.github.com>
2026-06-10 20:08:17 +0200

    small cleanup

--- src/FluentPassFinder/ViewModels/SettingsViewModel.cs
+++ src/FluentPassFinder/ViewModels/SettingsViewModel.cs
@@ -1,6 +1,5 @@
 // SPDX-FileCopyrightText: Copyright (C) 2023-2026 Uwe Koegel
 // SPDX-License-Identifier: GPL-3.0-or-later
-using System;
 using System.Collections.ObjectModel;
 using System.Reflection;
 using System.Text.RegularExpressions;
